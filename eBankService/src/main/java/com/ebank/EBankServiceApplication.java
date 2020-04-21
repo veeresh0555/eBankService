e1edfd62964840c16c7a21f1a15bba82d353449a
veeru <veeru@DESKTOP-EPVTAH2>
2020-04-21 16:55:36 +0530

    ribbonconfiguration

--- eBankService/src/main/java/com/ebank/EBankServiceApplication.java
+++ eBankService/src/main/java/com/ebank/EBankServiceApplication.java
@@ -3,9 +3,12 @@ package com.ebank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import com.ebank.config.RibbonConfiguration;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="ebankservice",configuration = RibbonConfiguration.class)
 public class EBankServiceApplication {
 
 	public static void main(String[] args) {
